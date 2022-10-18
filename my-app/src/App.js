17501f59b5e681599e19e9841cfb125c13782dba
vladislav <vl_evlashin@mail.ru>
2022-10-18 17:38:29 +0300

    Сделал первое дз

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,14 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
+import Message from './components/Message';
+
+const message = "Message"
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Message message={message} />
         <a
           className="App-link"
           href="https://reactjs.org"
